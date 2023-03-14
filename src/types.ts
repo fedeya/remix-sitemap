298592fb663583f5e3a1cfb888ade6aa81420d28
Federico Minaya <elfedeminaya@gmail.com>
2023-03-14 15:55:25 -0300

    fix: headers type

--- src/types.ts
+++ src/types.ts
@@ -13,7 +13,7 @@ export interface RemixSitemapConfig {
   sitemapBaseFileName?: string;
   alternateRefs?: AlternateRef[];
   optionalSegments?: Record<string, string[]>;
-  headers?: Headers;
+  headers?: Headers | Record<string, string>;
 }
 
 export interface SitemapEntry {
