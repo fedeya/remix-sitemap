95de3fe54c4856f8e891e326b4222552b1f264a7
Federico Minaya <elfedeminaya@gmail.com>
2023-03-13 22:51:02 -0300

    remove dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { EntryContext } from '@remix-run/server-runtime';
-import type { RemixSitemapConfig } from './types';
-export type { SitemapHandle, RemixSitemapConfig } from './types';
-export declare const createSitemapGenerator: (config: RemixSitemapConfig) => {
-    sitemap: (request: Request, context: EntryContext) => Promise<Response>;
-    isSitemapUrl(request: Request): boolean;
-};
