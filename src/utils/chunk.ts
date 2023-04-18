89d925fcf9f212f0db94534192070db440342994
Federico Minaya <elfedeminaya@gmail.com>
2023-04-18 20:57:59 -0300

    docs: add jsdoc to chunk function

--- src/utils/chunk.ts
+++ src/utils/chunk.ts
@@ -1,3 +1,16 @@
+/**
+ * Chunk an array into smaller arrays of a specified size.
+ *
+ * @param arr The array to chunk.
+ * @param chunkSize The size of each chunk.
+ *
+ * @returns An array of chunks.
+ *
+ * @see https://web.archive.org/web/20150909134228/https://jsperf.com/chunk-mtds
+ *
+ * @example
+ * chunk([1, 2, 3, 4, 5], 2) // [[1, 2], [3, 4], [5]]
+ */
 export function chunk<T>(arr: T[], chunkSize: number): T[][] {
   const R = [];
 
