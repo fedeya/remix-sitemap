cb9d4e69f7e017b2fbb54b5da1d2a1d1adb66f5c
Federico Minaya <hello@fedeminaya.com>
2023-08-29 08:43:17 -0700

    fix: get path of index route with parents (#49)

--- src/utils/__tests__/data.test.ts
+++ src/utils/__tests__/data.test.ts
@@ -36,4 +36,53 @@ describe('getFullPath', () => {
 
     expect(path).toBe('blog/post/:id');
   });
+
+  test('should return undefined in index route with same name parent', () => {
+    const path = getFullPath('blog._index', {
+      root: {
+        path: ''
+      },
+      blog: {
+        parentId: 'root',
+        path: 'blog'
+      },
+      'blog._index': {
+        parentId: 'blog'
+      }
+    });
+
+    expect(path).toBeUndefined();
+  });
+
+  test('should return path in index route without parent', () => {
+    const path = getFullPath('blog._index', {
+      root: {
+        path: ''
+      },
+      'blog._index': {
+        parentId: 'root',
+        path: 'blog'
+      }
+    });
+
+    expect(path).toBe('blog');
+  });
+
+  test('should return path with parents in index route', () => {
+    const path = getFullPath('admin.content._index', {
+      root: {
+        path: ''
+      },
+      admin: {
+        parentId: 'root',
+        path: 'admin'
+      },
+      'admin.content._index': {
+        parentId: 'admin',
+        path: 'content'
+      }
+    });
+
+    expect(path).toBe('admin/content');
+  });
 });
