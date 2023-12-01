8b03d531b3ae0b17ede638f16f633f4b706909c5
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-12-01 14:22:06 +1100

    +citations

--- src/utils/singleton.ts
+++ src/utils/singleton.ts
@@ -1,3 +1,6 @@
+// original code from https://github.com/jenseng/abuse-the-platform/blob/main/app/utils/singleton.ts
+// sourced from https://github.com/remix-run/blues-stack/blob/main/app/singleton.server.ts
+
 declare global {
   // must be var not let due to variable hoisting
   var __singletons: Record<string, unknown> | undefined; // eslint-disable-line
