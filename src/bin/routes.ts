bc6f8f4e083355251fa3c551e771309c63e6011a
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2025-09-29 21:32:02 +0530

    Refactor getRoutesAndModules function (#76)

--- src/bin/routes.ts
+++ src/bin/routes.ts
@@ -1,7 +1,4 @@
-import { readConfig } from '@remix-run/dev/dist/config.js';
 import path from 'path';
-import { createModuleBuilder } from './moduleBuilder';
-import requireFromString from 'require-from-string';
 import type { EntryContext } from '../lib/types';
 
 const dir = path.resolve(process.cwd());
@@ -9,29 +6,26 @@ const dir = path.resolve(process.cwd());
 type RouteModules = EntryContext['routeModules'];
 
 export async function getRoutesAndModules() {
-  const config = await readConfig(process.env.REMIX_ROOT || dir);
+  const remixRoot = process.env.REMIX_ROOT || dir;
 
-  const routes = config.routes;
+  const buildRoute = path.join(remixRoot, '/build/server/index.js');
+
+  const { routes } = await import(buildRoute);
 
   const modules: RouteModules = {};
 
   console.log('🔍 Found routes: ' + Object.keys(routes).join(', '));
 
-  const { buildModule } = await createModuleBuilder();
-
   await Promise.all(
     Object.keys(routes).map(async key => {
-      const route = routes[key];
-
       if (key === 'root') return;
 
-      const file = path.resolve(config.appDirectory, route.file);
+      const route = routes[key];
 
-      const result = await buildModule(file);
-
-      const module = requireFromString(result.outputFiles[0].text);
-
-      modules[key] = module;
+      if (route.module) {
+        modules[key] = route.module;
+        return;
+      }
     })
   );
 
